598d3fd6b7f79f630d469e952ffd402ee214ca54
marco <marco@DESKTOP-6H5A1CV>
2021-02-13 17:29:09 +0100

    commit demo

---
+++ TrabajoEEDD2Trimestre/src/demo/Demo.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @author MarcosMoralesAragon
+ **/
+public class Demo {
+	public static void main(String[] args) {
+
+		System.out.println("Hola mundo");
+
+	}
+}
---
+++ TrabajoEEDD2Trimestre/src/module-info.java
@@ -0,0 +1,2 @@
+module TrabajoEEDD2Trimestre {
+}
